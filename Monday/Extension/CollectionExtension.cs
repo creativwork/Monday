a5356df284c5d58e0a51c91aafb82cd76196fb90
Martin Matanovic <one@creativwork.org>
2019-12-09 14:33:53 +0100

    New Extension for collections.

---
+++ Monday/Extension/CollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Monday.Helper
+{
+    public static class CollectionExtension
+    {
+        public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+            => items.ToList().ForEach(collection.Add);
+
+        public static void AddRangeCleared<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            collection.Clear();
+            items.ToList().ForEach(collection.Add);
+        }
+    }
+}
